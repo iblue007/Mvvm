7dcdf8691f18f38912ff5c095e84a7773f463f5b
iblue007 <744639775@qq.com>
2021-07-09 15:09:34 +0800

    修改配置 实现processor的编译期间操作

--- libnavcompiler/src/main/java/com/example/libnavcompiler/NavProcessor.java
+++ libnavcompiler/src/main/java/com/example/libnavcompiler/NavProcessor.java
@@ -9,9 +9,9 @@ import com.google.auto.service.AutoService;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.Set;
 
@@ -19,6 +19,7 @@ import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.Filer;
 import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
@@ -38,63 +39,67 @@ import javax.tools.StandardLocation;
  * <p>
  * SupportedAnnotationTypes:声明该注解处理器想要处理那些注解
  */
-@AutoService(Process.class)
+@AutoService(Processor.class)
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
-@SupportedAnnotationTypes({"com.example.libnavannotation.ActivityDestination", "com.example.libnavannotation.ActivityDestination"})
+@SupportedAnnotationTypes({"com.example.libnavannotation.ActivityDestination", "com.example.libnavannotation.FragmentDestination"})
 public class NavProcessor extends AbstractProcessor {
-
     private Messager messager;
     private Filer filer;
     private static final String OUTPUT_FILE_NAME = "destination.json";
 
     @Override
-    public synchronized void init(ProcessingEnvironment processingEnvironment) {
-        super.init(processingEnvironment);
-        //日志打印，在java环境下不能使用android.util.log.e()
-        messager = processingEnvironment.getMessager();
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+        //日志打印,在java环境下不能使用android.util.log.e()
+        messager = processingEnv.getMessager();
         //文件处理工具
-        filer = processingEnvironment.getFiler();
+        filer = processingEnv.getFiler();
     }
 
     @Override
-    public Set<String> getSupportedAnnotationTypes() {
-        return super.getSupportedAnnotationTypes();
-    }
-
-    @Override
-    public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         //通过处理器环境上下文roundEnv分别获取 项目中标记的FragmentDestination.class 和ActivityDestination.class注解。
         //此目的就是为了收集项目中哪些类 被注解标记了
-        Set<? extends Element> fragmentElements = roundEnvironment.getElementsAnnotatedWith(FragmentDestination.class);
-        Set<? extends Element> activityElements = roundEnvironment.getElementsAnnotatedWith(ActivityDestination.class);
-
-        if (!fragmentElements.isEmpty() && !activityElements.isEmpty()) {
+        Set<? extends Element> fragmentElements = roundEnv.getElementsAnnotatedWith(FragmentDestination.class);
+        Set<? extends Element> activityElements = roundEnv.getElementsAnnotatedWith(ActivityDestination.class);
+        messager.printMessage(Diagnostic.Kind.NOTE, "resourcePath:00000000000000000");
+        if (!fragmentElements.isEmpty() || !activityElements.isEmpty()) {
             HashMap<String, JSONObject> destMap = new HashMap<>();
             //分别 处理FragmentDestination  和 ActivityDestination 注解类型
             //并收集到destMap 这个map中。以此就能记录下所有的页面信息了
             handleDestination(fragmentElements, FragmentDestination.class, destMap);
-            handleDestination(fragmentElements, ActivityDestination.class, destMap);
+            handleDestination(activityElements, ActivityDestination.class, destMap);
 
             //app/src/main/assets
             FileOutputStream fos = null;
             OutputStreamWriter writer = null;
             try {
-                FileObject resouce = filer.createResource(StandardLocation.CLASS_OUTPUT, "", OUTPUT_FILE_NAME);
-                String resourcePath = resouce.toUri().getPath();
-                messager.printMessage(Diagnostic.Kind.NOTE, "====resourcePath:" + resourcePath);
+                //filer.createResource()意思是创建源文件
+                //我们可以指定为class文件输出的地方，
+                //StandardLocation.CLASS_OUTPUT：java文件生成class文件的位置，/app/build/intermediates/javac/debug/classes/目录下
+                //StandardLocation.SOURCE_OUTPUT：java文件的位置，一般在/ppjoke/app/build/generated/source/apt/目录下
+                //StandardLocation.CLASS_PATH 和 StandardLocation.SOURCE_PATH用的不多，指的了这个参数，就要指定生成文件的pkg包名了
+                FileObject resource = filer.createResource(StandardLocation.CLASS_OUTPUT, "", OUTPUT_FILE_NAME);
+                String resourcePath = resource.toUri().getPath();
+                messager.printMessage(Diagnostic.Kind.NOTE, "resourcePath:" + resourcePath);
+
                 //由于我们想要把json文件生成在app/src/main/assets/目录下,所以这里可以对字符串做一个截取，
                 //以此便能准确获取项目在每个电脑上的 /app/src/main/assets/的路径
                 String appPath = resourcePath.substring(0, resourcePath.indexOf("app") + 4);
                 String assetsPath = appPath + "src/main/assets/";
+
                 File file = new File(assetsPath);
                 if (!file.exists()) {
                     file.mkdirs();
                 }
+
+                //此处就是稳健的写入了
                 File outPutFile = new File(file, OUTPUT_FILE_NAME);
                 if (outPutFile.exists()) {
                     outPutFile.delete();
                 }
                 outPutFile.createNewFile();
+
                 //利用fastjson把收集到的所有的页面信息 转换成JSON格式的。并输出到文件中
                 String content = JSON.toJSONString(destMap);
                 fos = new FileOutputStream(outPutFile);
@@ -104,14 +109,15 @@ public class NavProcessor extends AbstractProcessor {
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
-                if(writer != null){
+                if (writer != null) {
                     try {
                         writer.close();
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
-                if(fos != null){
+
+                if (fos != null) {
                     try {
                         fos.close();
                     } catch (IOException e) {
@@ -120,6 +126,8 @@ public class NavProcessor extends AbstractProcessor {
                 }
             }
         }
+
+
         return true;
     }
 
@@ -128,7 +136,7 @@ public class NavProcessor extends AbstractProcessor {
             //TypeElement是Element的一种。
             //如果我们的注解标记在了类名上。所以可以直接强转一下。使用它得到全类名
             TypeElement typeElement = (TypeElement) element;
-            //全类名
+            //全类名com.mooc.ppjoke.home
             String clazName = typeElement.getQualifiedName().toString();
             //页面的id.此处不能重复,使用页面的类名做hascode即可
             int id = Math.abs(clazName.hashCode());
@@ -143,33 +151,31 @@ public class NavProcessor extends AbstractProcessor {
 
             Annotation annotation = element.getAnnotation(annotationClaz);
             if (annotation instanceof FragmentDestination) {
-                FragmentDestination fragmentDestination = (FragmentDestination) annotation;
-                pageUrl = fragmentDestination.pageUrl();
-                needLogin = fragmentDestination.needLogin();
-                asStarter = fragmentDestination.asStarter();
+                FragmentDestination dest = (FragmentDestination) annotation;
+                pageUrl = dest.pageUrl();
+                asStarter = dest.asStarter();
+                needLogin = dest.needLogin();
                 isFragment = true;
             } else if (annotation instanceof ActivityDestination) {
-                ActivityDestination activityDestination = (ActivityDestination) annotation;
-                pageUrl = activityDestination.pageUrl();
-                needLogin = activityDestination.needLogin();
-                asStarter = activityDestination.asStarter();
+                ActivityDestination dest = (ActivityDestination) annotation;
+                pageUrl = dest.pageUrl();
+                asStarter = dest.asStarter();
+                needLogin = dest.needLogin();
                 isFragment = false;
             }
 
             if (destMap.containsKey(pageUrl)) {
-                messager.printMessage(Diagnostic.Kind.ERROR, "不同的页面不允许使用相同的pageurl:" + clazName);
+                messager.printMessage(Diagnostic.Kind.ERROR, "不同的页面不允许使用相同的pageUrl：" + clazName);
             } else {
                 JSONObject object = new JSONObject();
                 object.put("id", id);
                 object.put("needLogin", needLogin);
-                object.put("pageUrl", pageUrl);
                 object.put("asStarter", asStarter);
-                object.put("isFragment", isFragment);
+                object.put("pageUrl", pageUrl);
                 object.put("className", clazName);
+                object.put("isFragment", isFragment);
                 destMap.put(pageUrl, object);
             }
         }
     }
-
-
 }
